0d59239adfb64d2d2b822db34e02c49f4fedaa64
des-v <desireevaldes@gmail.com>
2021-10-25 18:02:30 +0200

    Add example.js file

---
+++ example.js
@@ -0,0 +1,7 @@
+// Generate a random dot6 braille characters from a given amount of characters.
+// Usage example:
+const brailleRandom = require('braille-random')
+
+brailleRandom(3) // Call function with a
+
+console.log(brailleRandom(3)) // random print: ⠭⠒⠇
